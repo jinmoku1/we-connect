039142e829d63aa18c1321974c3516675388e248
rococori <drfy55@gmail.com>
2014-02-13 14:31:13 -0600

    First draft code to connect mongoDB from node.js
    insertion only.

--- lab/helloworld.js
+++ lab/helloworld.js
@@ -1,5 +1,22 @@
-/**
- * New node file
- */
+var MongoClient = require('mongodb');
 
-console.log("hello world!");
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/mydb", function(err, db){
+    if(err) { return console.dir(err); }
+
+    //db.createCollection('testData', {strict:true}, function(err, collection){});
+    //db.createCollection('testData2', {strict:true}, function(err, collection){});
+
+    var collection = db.collection('testData');
+    var doc1 = {'hello' : 'doc1'};
+    var doc2 = {'hello' : 'doc2'};
+    var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+
+    // This somewhat works.
+    collection.insert(doc1, {w:1}, function(err, result){});
+
+});
+
+
+
+console.log("Hello World");
