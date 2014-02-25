fcd91abb20d2ec07ccdcd012b6838d1cddec6ce0
Seungchul Lee <sclee8611@gmail.com>
2014-02-24 18:49:05 -0600

    Comment in sample.js is updated

--- app/tests/sample.js
+++ app/tests/sample.js
@@ -1,7 +1,7 @@
 /**
  * You must have installed mocha in global level, use following commands to do so:
  * 
- * 	sudo install -g mocha
+ * 	sudo npm install -g mocha
  * 
  * Then you can run the test with following command:
  * 
