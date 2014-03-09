d154c632462862eb66f4991e9ff5afed8ef18909
Seung H. Cha <phonpr@outlook.com>
2014-03-09 14:42:37 -0500

    mongodb access and testing

--- lab/mongodb_test/main.js
+++ lab/mongodb_test/main.js
@@ -4,8 +4,12 @@
 
 db = require('./db');
 
-//db.put({ netId: 'scha3', password: '1234'});
+db.put({ netId: 'scha3', password: '1234' }, function(doc) {
+	console.log(doc[0]['_id']);
+});
 
+/*
 db.getByNetId('scha3', function(data) {
 	console.log(data);
 });
+*/
