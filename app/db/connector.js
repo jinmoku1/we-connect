52693e60b9f93bbaff99fee39ab3d390f9781e8c
Seung H. Cha <phonpr@outlook.com>
2014-03-09 16:20:21 -0500

    db/account to db/accountDb with refacotring

--- app/db/connector.js
+++ app/db/connector.js
@@ -2,10 +2,43 @@
  * 
  */
 
-var MongoClient = require('mongodb').MongoClient;
+// Database Configuration
+var CONNECTION_STRING = 'mongodb://hay.synology.me:27017/weconnect';
+
+var mongoClient = require('mongodb').MongoClient;
 
 exports.connect = function(callback) {
-	MongoClient.connect('mongodb://hay.synology.me:27017/weconnect', function(err, db) {
-		callback(err, db);
+	mongoClient.connect(CONNECTION_STRING, function(err, db) {
+		if (err) {
+			console.log("[ERROR] Database Connection failed : " + CONNECTION_STRING);
+			return;
+		}
+		callback(db);
+	});
+};
+
+exports.save = function(collection, inputDoc, callback) {
+	exports.connect(function(db) {
+		var dbCollection = db.collection(collection);
+		dbCollection.save(inputDoc, {safe:true}, function(err, resultDoc) {
+			if (err) {
+				console.log("[ERROR] Save on \'"+collection+"\' Failed.");
+				return;
+			}
+			callback(db, resultDoc);
+		});
+	});
+};
+
+exports.findOne = function(collection, conditionDoc, callback) {
+	exports.connect(function(db) {
+		var dbCollection = db.collection(collection);
+		dbCollection.findOne(conditionDoc, {safe:true}, function(err, resultDoc) {
+			if (err) {
+				console.log("[ERROR] Find on \'"+collection+"\' Faield.");
+				return;
+			}
+			callback(db, resultDoc);
+		});
 	});
 };
