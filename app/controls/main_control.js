688502f8f6cb28a1a6ee277318bd24c1bc24ae4b
jinmoku1 <jinmoku1@gmail.com>
2014-03-15 16:43:46 -0500

    error

--- app/controls/main_control.js
+++ app/controls/main_control.js
@@ -14,7 +14,7 @@ exports.index = function(req, res) {
 	}
 	
 	res.render('index', {
-		user : session.getSessionUser(),
+		user : session.getSessionUsers(),
 		userConst : userConst,
 		title : 'WeConnect : CS',
 		welcome : 'Welcome to WeConnect'
