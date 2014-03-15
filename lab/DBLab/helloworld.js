21227071e6787fb9c8250b16d162dd8b0f92684f
rococori <drfy55@gmail.com>
2014-03-15 17:32:49 -0500

    no message

--- lab/DBLab/helloworld.js
+++
@@ -1,9 +0,0 @@
-var MongoClient = require('mongodb');
-
-// Connect to the db
-MongoClient.connect("mongodb://hay.synology.me:27017", function(err, db){
-	if(!err) {
-		console.log("Hello, World");
-	}
-});
-
