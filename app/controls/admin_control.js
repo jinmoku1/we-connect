b29df05edfa8364e84974c60949ccbd36fb5eb37
jinmoku1 <jinmoku1@gmail.com>
2014-04-27 02:10:36 -0500

    admin

---
+++ app/controls/admin_control.js
@@ -0,0 +1,53 @@
+/*
+ * GET (non-view) resources
+ */
+
+var session = require('../session');
+var userDb = require('../db/user_db');
+var anncDb = require('../db/annc_db');
+var ObjectID = require('mongodb').ObjectID;
+var constants = require('../constants');
+
+exports.approve = function(req, res) {
+	var user = session.getSessionUser(req);
+	var anncId = req.body.id;
+	var id = ObjectID.createFromHexString(anncId);
+	
+	if (user.userType == constants.user.TYPE_ADMIN){
+		anncDb.getDetail(id, function(anncDetail) {
+			anncDetail.status = constants.annc.ACCEPTED;
+			anncDb.updateInfo(id, anncDetail, function(success){
+				if (success){
+					res.writeHead(200, {"Content-Type": "text/plain"});
+					res.end("true");
+				}
+				else {
+					res.writeHead(200, {"Content-Type": "text/plain"});
+					res.end("false");
+				}
+			});
+		});
+	}
+};
+
+exports.disapprove = function(req, res) {
+	var user = session.getSessionUser(req);
+	var anncId = req.body.id;
+	var id = ObjectID.createFromHexString(anncId);
+	
+	if (user.userType == constants.user.TYPE_ADMIN){
+		anncDb.getDetail(id, function(anncDetail) {
+			anncDetail.status = constants.annc.REJECTED;
+			anncDb.updateInfo(id, anncDetail, function(success){
+				if (success){
+					res.writeHead(200, {"Content-Type": "text/plain"});
+					res.end("true");
+				}
+				else {
+					res.writeHead(200, {"Content-Type": "text/plain"});
+					res.end("false");
+				}
+			});
+		});
+	}
+};
